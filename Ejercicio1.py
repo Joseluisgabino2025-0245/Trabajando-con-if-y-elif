1163216c9537f6a3e90960ce70450651fb09e69d
Jose Luis Gabino 2025-0245 <2025-0245@unad.edu.do>
2025-10-19 21:53:13 -0400

    Update Ejercicio1.py

---
+++ Ejercicio1.py
@@ -0,0 +1,10 @@
+#Número positivo, negativo o cero
+
+numero = float(input("Ingresa un número por favor: "))
+
+if numero > 0:
+    print("El número que ingresaste es positivo es positivo.")
+elif numero < 0:
+    print("El número que ingresaste es negativo.")
+else:
+    print("El número que ingresaste es cero.")
