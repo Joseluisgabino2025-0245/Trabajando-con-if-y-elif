c4fcc0683cf9e89049d9fe36e34d55f4a22af90d
Jose Luis Gabino 2025-0245 <2025-0245@unad.edu.do>
2025-10-19 22:01:08 -0400

    Ejercicio7.py

---
+++ Ejercicio7.py
@@ -0,0 +1,27 @@
+#Día de la semana
+
+numero = int(input("Digite un número del 1 al 7: "))
+
+if numero == 1:
+    print("El dia es lunes")
+
+elif numero == 2:
+    print("El dia es martes")
+
+elif numero == 3:
+    print("El dia es miercoles")
+
+elif numero == 4:
+    print("El dia es jueves")
+
+elif numero == 5:
+    print("el dia es viernes")
+
+elif numero == 6:
+    print("El dia es sabado")
+
+elif numero == 7:
+    print("El dia es domingo")
+
+else:
+    print("Número inválido. Debe ser del 1 al 7.")
