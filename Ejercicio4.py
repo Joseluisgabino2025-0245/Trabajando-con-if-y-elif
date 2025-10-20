7080edca553e9cc0602dc860f7f73779befd14ab
Jose Luis Gabino 2025-0245 <2025-0245@unad.edu.do>
2025-10-19 21:58:59 -0400

    Ejercicio4.py

---
+++ Ejercicio4.py
@@ -0,0 +1,8 @@
+#Múltiplo de 5
+
+Numero = int(input("Por favor de ingresar un número: "))
+
+if Numero % 5 == 0:
+    print("El número es múltiplo de 5.")
+else:
+    print("El número no es múltiplo de 5.")
