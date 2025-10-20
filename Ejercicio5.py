e6e5c28320572f51eaa9c11b236fa58d9d7816f2
Jose Luis Gabino 2025-0245 <2025-0245@unad.edu.do>
2025-10-19 21:59:39 -0400

    Ejercicio5.py

---
+++ Ejercicio5.py
@@ -0,0 +1,8 @@
+#Descuento por edad
+
+edad = int(input("Ingresa tu edad correcta por favor: "))
+
+if edad >= 60:
+    print("Aplicas para un descuento del 50%.")
+else:
+    print("No aplicas para el descuento.")
