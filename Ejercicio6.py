c4fe1f94efdaabb1b38ed7cb295265d3fa7e8ba6
Jose Luis Gabino 2025-0245 <2025-0245@unad.edu.do>
2025-10-19 22:00:18 -0400

    Ejercicio6.py

---
+++ Ejercicio6.py
@@ -0,0 +1,8 @@
+#Calificación aprobatoria
+
+Calificacion = float(input("Ingresa tu calificación escolar (0-100): "))
+
+if Calificacion >= 60:
+    print("Aprobaste.")
+else:
+    print("Reprobaste.")
