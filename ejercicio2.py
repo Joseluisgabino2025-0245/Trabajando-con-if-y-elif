078fa69ec341c1ffdfc3a84a1aba7971de086a7c
Jose Luis Gabino 2025-0245 <2025-0245@unad.edu.do>
2025-10-19 21:56:35 -0400

    Ejercicio2.py

---
+++ ejercicio2.py
@@ -0,0 +1,8 @@
+#Número par o impar
+
+numero = int(input("Digite un número entero: "))
+
+if numero % 2 == 0:
+    print("El número que ingreso es par.")
+else:
+    print("El número que ingreso es impar.")
