bf4226b634aaac972f9263aed6827d9ce60bca8c
Jose Luis Gabino 2025-0245 <2025-0245@unad.edu.do>
2025-10-19 22:01:50 -0400

    Ejercicio8.py

---
+++ Ejercicio8.py
@@ -0,0 +1,13 @@
+#Número mayor entre dos
+
+numr1 = float(input("Digite el primer número: "))
+numr2 = float(input("Digite el segundo número: "))
+
+if numr1 > numr2:
+    print("El primer número es mayor.")
+    
+elif numr2 > numr1:
+    print("El segundo número es mayor.")
+
+else:
+    print("Ambos números son iguales.")
